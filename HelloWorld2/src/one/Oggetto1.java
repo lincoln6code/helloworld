b48b534cf5177fd3d44cb0d665db2c72f4387ec4
GiacomoL <giacomolvrt@gmail.com>
2018-05-25 11:40:14 +0200

    Aggiunte prints

--- HelloWorld2/src/one/Oggetto1.java
+++ HelloWorld2/src/one/Oggetto1.java
@@ -5,10 +5,12 @@ public class Oggetto1 {
 	private int numero_pezzi;
 
 	public int getNumero_pezzi() {
+		System.out.println("getNumero_pezzi");
 		return numero_pezzi;
 	}
 
 	public void setNumero_pezzi(int numero_pezzi) {
+		System.out.println("setNumero_pezzi");
 		this.numero_pezzi = numero_pezzi;
 	}
 	
